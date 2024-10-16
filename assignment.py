40122ceca7dafa357badfff5d343c6c5da156e1e
Silvereagle8 <168670142+Silvereagle8@users.noreply.github.com>
2024-10-16 10:49:05 -0600

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,18 @@
+string_variable = 'string variable'
+number_variable = 4
+list_variable = ['Element0', 'Element1', 'Element2']
+bool_variable = False
+
+first_three_letters = string_variable[:3]
+first_element = list_variable[0]
+new_number = number_variable + 10
+last_element = list_variable[-1]
+
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+
+uppercase_string = string_variable[:6].upper()
+full_string = uppercase_string + string_variable[6:]
+
+print(f'My number is {number_variable}')
+print('hello world')
